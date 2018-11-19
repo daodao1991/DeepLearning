9a6a213dc83fcfdd2fb49372ac0c43af35fe987d
daodao1991 <471229015@qq.com>
2018-11-19 11:42:39 +0800

    update train_AlexNet.py

--- RCNN/train_AlexNet.py
+++ RCNN/train_AlexNet.py
@@ -45,5 +45,56 @@ def load_from_pkl(dataset_file):
     return X, Y
 
 
-# 创建网络 AlexNet
 def create_AlexNet(num_classes):
+    ''' 创建网络 AlexNet '''
+    network = input_data(shape=[None, config.IMAGE_SIZE, config.IMAGE_SIZE, 3])
+    network = conv_2d(network, 96, 11, strides=4, activation='relu')
+    network = max_pool_2d(network, 3, strides=2)
+    network = local_response_normalization(network)
+    network = conv_2d(network, 256, 5, activation='relu')
+    network = max_pool_2d(network, 3, strides=2)
+    network = local_response_normalization(network)
+    network = conv_2d(network, 384, 3, activation='relu')
+    network = conv_2d(network, 384, 3, activation='relu')
+    network = conv_2d(network, 256, 3, activation='relu')
+    network = max_pool_2d(network, 3, strides=2)
+    network = local_response_normalization(network)
+    network = fully_connected(network, 4096, activation='tanh')
+    network = dropout(network, 0.5)
+    network = fully_connected(network, 4096, activation='tanh')
+    network = dropout(network, 0.5)
+    network = fully_connected(network, num_classes, activation='softmax')
+    network = regression(network, optimizer='momentum', 
+                    loss='categorical_crossentropy', 
+                    learning_rate=0.001)
+    return network
+
+
+def train(network, X, Y, save_model_path):
+    '''训练网络'''
+    model = tflearn.DNN(network, checkpoint_path='model_alexnetwork', 
+                        max_checkpoints=1, tensorboard_verbose=2,
+                        tensorboard_dir='output')
+    if os.path.isfile(save_model_path + '.index'):
+        model.load(save_model_path)
+        print('load model...')
+    for _ in range(5):
+        model.fit(X, Y, n_epoch=1000, validation_set=0.1, shuffle=True, 
+                  show_metric=True, batch_size=64, snapshot_step=200, 
+                  snapshot_epoch=False,
+                  run_id='alexnetwork_oxflowers17')
+        # Save the model
+        model.save(save_model_path)
+        print('save model...')
+
+
+def predict(network, modelfile, images):
+        model = tflearn.DNN(network)
+        model.load(modelfile)
+        return model.predict(images)
+
+
+if _name__ == '__main__':
+    X, Y = load_data(config.TRAIN_LIST, config.TRAIN_CLASS)
+    net = create_AlexNet(config.TRAIN_CLASS)
+    train(net, X, Y, config.SAVE_MODEL_PATH)
